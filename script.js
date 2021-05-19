6b86fe2f1d42a3beefe3f8b0bd2c6d95e861a847
Mathieu Cossutta <mcossutta@gmail.com>
2021-05-19 16:29:54 +0000

    add scripting

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal");
+    if (principal.value <= 0) {
+    alert("Enter a positive number");
+    principal.focus();}
+    else{
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal.value * years * rate/100 ;
+    var year = new Date().getFullYear()+parseInt(years);
+    document.getElementById("result").innerHTML="If you deposit \<mark\>"+principal.value+"\<\/mark\>,\<br\>at an interest rate of \<mark\>"+rate+"%\<\/mark\>.\<br\>You will receive an amount of \<mark\>"+interest+"\<\/mark\>,\<br\>in the year \<mark\>"+year+"\<\/mark\>.\<br\>";
+    }
+}
+
+function updateRate()
+{
+
+document.getElementById("rate_val").innerText = document.getElementById("rate").value
 }
         
