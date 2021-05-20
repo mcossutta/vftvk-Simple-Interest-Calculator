8f288a0cb443868ad5536a7d67357c0013771c28
mcossutta <mcossutta@gmail.com>
2021-05-20 12:35:51 +0200

    table for the form

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ function compute()
     // else compute the interest and display the result in the element with id result
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
+    console.log(years)
     var interest = principal.value * years * rate/100 ;
     var year = new Date().getFullYear()+parseInt(years);
     document.getElementById("result").innerHTML="If you deposit \<mark\>"+principal.value+"\<\/mark\>,\<br\>at an interest rate of \<mark\>"+rate+"%\<\/mark\>.\<br\>You will receive an amount of \<mark\>"+interest+"\<\/mark\>,\<br\>in the year \<mark\>"+year+"\<\/mark\>.\<br\>";
@@ -19,6 +20,6 @@ function compute()
 function updateRate()
 {
 
-document.getElementById("rate_val").innerText = document.getElementById("rate").value
+document.getElementById("rate_val").innerText = document.getElementById("rate").value+"%"
 }
         
